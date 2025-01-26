f99e802f40c2cb58f5e84c023dd88a626d546e86
zbransi <zoebransi@gmail.com>
2025-01-26 14:13:45 -0800

    Delete relax.js

--- relax.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    setTimeout(function() {
-        window.location.href = "focus.html"; 
-    }, 20000);
-//300000
-    alert("Time to work!"); // triggers the callback after 5 mins
-};
